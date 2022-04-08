ead16e59355c68cca040704829ddf64bba05e7e3
Danielle <danibarb@gmail.com>
2022-04-07 22:30:31 -0300

    Add files via upload
    Classe com modificador Internal.
    Documentação do sistema

---
+++ ParceiroComercial.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos.Properties;
+
+namespace ByteBank.Modelos
+{
+    internal class ParceiroComercial : IAutenticavel
+    {
+        public string Senha { get; set; }
+
+        public AutenticacaoHelper autenticacaoHelper = new AutenticacaoHelper();
+
+        public bool Autenticar(string senha)
+        {
+            return autenticacaoHelper.CompararSenha(Senha, senha);
+        }
+    }
+}
